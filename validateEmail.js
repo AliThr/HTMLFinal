9ab4fa53f0427d3af572d2a2d2fd9b1960592572
Alister Thrush <alister.thrush@dootrix.com>
2018-04-25 11:05:48 +0100

    Fix issue where popup would reload regardless of pass or fail
    To do this, I actually called the 'Close' function. I also separated the javascript functions up to make it clearer as to what is happening and so each function only does one thing.

--- validateEmail.js
+++ validateEmail.js
@@ -1,15 +1,30 @@
-function ValidateAndClose(inputText){  
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
-    if(inputText.value.match(mailformat)){  
+function ValidateAndClose(event){  
+    let name = document.getElementById("inputName")
+    let email = document.getElementById("email")
+    if(ValidateName(name) && ValidateEmail(email)){  
         alert("Thank you! You are now subscribed to our news letters!")
-        return true; 
+        Close(); 
+
     } else {  
-        alert("You have entered an invalid email address!");  
-        return false;  
+        alert("You have entered an invalid name or email address!");  
+        return true;  
     }
 }  
-function close(ValidateAndClose){
-    if (ValidateAndClose === true){
-        document.getElementById("popup1").style.display = "none";
+function ValidateName(inputText){
+    console.log(inputText);
+    if(inputText != ""){
+        return true;
     }
+    return false;
+}
+function ValidateEmail(inputText){
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+    if(inputText.value.match(mailformat)){  
+        return true;
+    }
+    return false;
+}
+
+function Close(){
+        document.getElementById("popup1").style.display = "none";
 }
